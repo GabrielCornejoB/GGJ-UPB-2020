241d2274f83a82ee8bad9a460c004108a3f8546d
Andrés Felipe Cano <25773235+AndresFelipeCano@users.noreply.github.com>
2020-02-01 07:44:42 -0500

    Arreglo de saltos múltiples

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -30,7 +30,7 @@ public class PlayerController : MonoBehaviour
 
     void Jump()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && !isJumping)
         {          
             rb.AddForce(new Vector2(rb.velocity.x, jumpForce));
             isJumping = true;
