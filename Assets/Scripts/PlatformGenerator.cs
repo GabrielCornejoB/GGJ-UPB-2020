1de1a58ec7baf056afe5562248da28a1ce08b039
Andrés Felipe Cano <25773235+AndresFelipeCano@users.noreply.github.com>
2020-02-01 22:46:16 -0500

    Random Stages

--- Assets/Scripts/PlatformGenerator.cs
+++ Assets/Scripts/PlatformGenerator.cs
@@ -4,26 +4,41 @@ using UnityEngine;
 
 public class PlatformGenerator : MonoBehaviour
 {
-    // public GameObject thePlatform;
-    public GameObject theStage;
-    public Transform generationPoint;
-    // public float distanceBetween;
-    private float platformWidth;
-    private float stageWidth;
+  public GameObject theStage;
+  public Transform generationPoint;
+  private float platformWidth;
+  private float stageWidth;
+  public GameObject[] stages;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        // platformWidth = thePlatform.GetComponent<BoxCollider2D>().size.x;
-        stageWidth = theStage.GetComponent<BoxCollider2D>().size.x;
+  // Start is called before the first frame update
+  void Start()
+  {
+    stageWidth = theStage.GetComponent<BoxCollider2D>().size.x;
+  }
+
+  public void SetInScene()
+  {
+    GameObject[] stages = new GameObject[2];
+    stages[0] = Resources.Load <GameObject>("Stages/Stage1");
+    stages[1] = Resources.Load <GameObject>("Stages/Stage2");
     }
 
-    // Update is called once per frame
-    void Update()
+  // Update is called once per frame
+  void Update()
+  {
+    if (transform.position.x < generationPoint.position.x)
     {
-        if (transform.position.x < generationPoint.position.x) {
-            transform.position = new Vector3(transform.position.x + stageWidth, transform.position.y, transform.position.z);
-            Instantiate(theStage, transform.position, transform.rotation);
-        }
+      GameObject nextStage = stages[Random.Range(0, stages.Length - 1)];
+      transform.position = new Vector3(transform.position.x + stageWidth, transform.position.y, transform.position.z);
+    //   Component[] generators = theStage.GetComponentsInChildren<PlatformGenerator>();
+    //   foreach (PlatformGenerator generator in generators)
+    //   {
+    //     generator.theStage = nextStage;
+    //     generator.generationPoint = generationPoint;
+    //     generator.stages = stages;
+    //   }
+      GameObject currentStage = Instantiate(theStage, transform.position, transform.rotation);
+      currentStage.GetComponentInChildren<PlatformGenerator>().SetInScene();
     }
+  }
 }
